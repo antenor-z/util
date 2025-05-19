cce517eb880fcc0b30b0c94489998d3f91b3d83c
antenor.leal <antenor.leal@oceanpact.com>
2025-05-19 13:08:16 -0300

    .

--- util.go
+++ util.go
@@ -1,11 +1,14 @@
 package main
 
-import "os"
+import (
+	"os"
+	"strings"
+)
 
 func GetHost() string {
 	contentB, err := os.ReadFile("host.txt")
 	if err != nil {
 		return "null"
 	}
-	return string(contentB)
+	return strings.TrimSpace(string(contentB))
 }
