4d0671ccf6192cda81a9e8eb7f42218601fcff61
antenor-z <antenor-z@pm.me>
2025-11-12 18:16:19 -0300

    fix: remove logs

--- middle/cache.go
+++ middle/cache.go
@@ -47,19 +47,16 @@ func (c *ExpirableCache) Get(key string) (any, bool) {
 	c.mutex.RUnlock()
 
 	if !ok {
-		println("cache miss", key)
 		return "", false
 	}
 
 	if v.ttl > 0 && time.Since(v.createdOn) > v.ttl {
-		println("cache miss", key)
 		c.mutex.Lock()
 		delete(c.cacheMap, key)
 		c.mutex.Unlock()
 		return "", false
 	}
 
-	println("cache hit", key)
 	return v.value, true
 }
 
